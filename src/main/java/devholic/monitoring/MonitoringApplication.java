1985d2a99b4d3751115dc553a4c9b020c907197c
Hyunjoon Choi <hyunjoon.tech@gmail.com>
2023-08-19 20:38:01 +0900

    initial commit

---
+++ src/main/java/devholic/monitoring/MonitoringApplication.java
@@ -0,0 +1,13 @@
+package devholic.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
+
+}
